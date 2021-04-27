bf413e47d0d6b7464e6a609abc8d5fdcdac41ac0
Jaime <fhjaime96@gmail.com>
2021-04-27 20:37:14 +0200

    Refactor for cache handler

--- .github/queryspotify.py
+++ .github/queryspotify.py
@@ -4,34 +4,51 @@ import json
 import os
 from github import Github
 
+
 class SecretsCacheHandler(CacheHandler):
-    def __init__(self, repository_name, github_access_token=None, github_access_token_secret_name=None, secret_cache_name="SPOTIPY_CACHE"):
+    def __init__(
+        self,
+        repository_name,
+        github_access_token=None,
+        github_access_token_secret_name=None,
+        secret_cache_name="SPOTIPY_CACHE",
+    ):
         CacheHandler.__init__(self)
-        #Create github api client from token or secret name containing token
-        if(github_access_token!=None):
+        # Create github api client from token or secret name containing token
+        if github_access_token != None:
             self.github_client = Github(github_access_token)
-        elif(github_access_token_secret_name!=None):
+        elif github_access_token_secret_name != None:
             self.github_client = Github(os.environ[github_access_token_secret_name])
         else:
-            raise Exception("You must provide token or secret name containing your github access token in order to instantiate the cache handler")
+            raise Exception(
+                "You must provide token or secret name containing your github access token in order to instantiate the cache handler"
+            )
 
         self.secret_cache_name = secret_cache_name
-        self.repository = self.github_client.get_repo(repository_name)
+        self.repository_name = repository_name
+
+    @property
+    def repo(self):
+        if self._repo is None:
+            self._repo = self.github_client.get_repo(self.repository_name)
+        return self._repo
 
     def save_token_to_cache(self, token_info):
+        self.token_info = token_info
+
         # `token_info` will be a python dict. convert it to a JSON string.
         token_string = json.dumps(token_info)
         # save the `token_string` to your secrets
-        self.repository.create_secret(secret_name=self.secret_cache_name,unencrypted_value=token_string)
+        self._repo.create_secret(
+            secret_name=self.secret_cache_name, unencrypted_value=token_string
+        )
 
     def get_cached_token(self):
-        #Retrieve secret from enviorment
-        token_string = os.environ[self.secret_cache_name]
+        return self.token_info
 
-        # Convert the string back to a dict and return it
-        token_info = json.loads(token_string)
-
-        return token_info
+    def _debug_make_token_expired(self):
+        # print("_test_make_token_expired")
+        self.token_info["expires_at"] = int(time.time())
 
 
 username = "jiwidi"
@@ -40,7 +57,10 @@ scope = "user-read-recently-played"
 spotify = Spotify(
     auth_manager=SpotifyOAuth(
         scope=scope,
-        cache_handler=SecretsCacheHandler(repository_name="jiwidi/jiwidi.github.io", github_access_token_secret_name="MAIN_TOKEN"),
+        cache_handler=SecretsCacheHandler(
+            repository_name="jiwidi/jiwidi.github.io",
+            github_access_token_secret_name="MAIN_TOKEN",
+        ),
     )
 )
 
@@ -48,4 +68,5 @@ results = spotify.current_user_recently_played()
 results["date_run"] = time.time()
 
 with open(".github/spotify_query_results.json", "w+") as f:
-    json.dump(results,f)
+    json.dump(results, f)
+
